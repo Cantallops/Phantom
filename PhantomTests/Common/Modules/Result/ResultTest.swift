d7c2122925f11179dcc654bc28c20e2326dca32e
Alberto Cantallops <alberto.cantallops@gmail.com>
2018-01-25 19:56:41 +0100

    :white_check_mark: Complete tests for Result

--- PhantomTests/Common/Modules/Result/ResultTest.swift
+++ PhantomTests/Common/Modules/Result/ResultTest.swift
@@ -48,4 +48,60 @@ class ResultTest: XCTestCase {
         XCTAssertFalse(result.isSuccess)
         XCTAssertTrue(result.isFailure)
     }
+
+    func testGetValue() {
+        let expectedResult = "Hi"
+        let result = Result.success(expectedResult)
+        XCTAssertEqual(result.value, expectedResult)
+        XCTAssertNil(result.error)
+    }
+
+    func testGetError() {
+        let error = TestError()
+        let result = Result<String>.failure(error)
+        XCTAssertNotNil(result.error)
+        XCTAssertNil(result.value)
+    }
+
+    func testCombinedResultSuccessAndSuccess() {
+        let expectedResult1 = "1"
+        let expectedResult2 = "2"
+        let result1 = Result.success(expectedResult1)
+        let result2 = Result.success(expectedResult2)
+        let combinedResult = result1.combined(result: result2)
+        XCTAssertTrue(combinedResult.isSuccess)
+        XCTAssertEqual(combinedResult.value!.0, expectedResult1)
+        XCTAssertEqual(combinedResult.value!.1, expectedResult2)
+    }
+
+    func testCombinedResultSuccessAndError() {
+        let expectedResult1 = "1"
+        let error = TestError()
+        let result1 = Result.success(expectedResult1)
+        let result2 = Result<String>.failure(error)
+        let combinedResult = result1.combined(result: result2)
+        XCTAssertTrue(combinedResult.isFailure)
+        XCTAssertNotNil(combinedResult.error)
+    }
+
+    func testCombinedResultErrorAndSuccess() {
+        let expectedResult2 = "2"
+        let error = TestError()
+        let result1 = Result<String>.failure(error)
+        let result2 = Result.success(expectedResult2)
+        let combinedResult = result1.combined(result: result2)
+        XCTAssertTrue(combinedResult.isFailure)
+        XCTAssertNotNil(combinedResult.error)
+    }
+
+    func testCombinedResultErrorAndError() {
+        let error1 = TestError(localizedDescription: "Error 1")
+        let error2 = TestError(localizedDescription: "Error 2")
+        let result1 = Result<String>.failure(error1)
+        let result2 = Result<String>.failure(error2)
+        let combinedResult = result1.combined(result: result2)
+        XCTAssertTrue(combinedResult.isFailure)
+        XCTAssertTrue(combinedResult.error is CombinedError)
+        XCTAssertEqual(combinedResult.error?.localizedDescription, "Error 1\nError 2")
+    }
 }
