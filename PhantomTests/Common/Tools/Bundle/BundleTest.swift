59dc4e2ad2346a1aaa0ee221bcc4854d0a7d2a9b
Alberto Cantallops <alberto.cantallops@gmail.com>
2018-02-19 18:11:35 +0100

    :white_check_mark: Fix test

--- PhantomTests/Common/Tools/Bundle/BundleTest.swift
+++ PhantomTests/Common/Tools/Bundle/BundleTest.swift
@@ -23,10 +23,10 @@ class BundleTest: XCTestCase {
     }
 
     func testVersionNumber() {
-        XCTAssertEqual(bundle.versionNumber, "1.0.3")
+        XCTAssertEqual(bundle.versionNumber, "1.0.4")
     }
 
     func testBuildNumber() {
-        XCTAssertEqual(bundle.buildNumber, "1.0.3.1")
+        XCTAssertEqual(bundle.buildNumber, "1.0.4.0")
     }
 }
