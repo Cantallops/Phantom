6e66db3be0c324cb62ce50e2ce3de55b89e6ecc9
Alberto Cantallops <alberto.cantallops@gmail.com>
2018-05-30 22:16:38 +0200

    :bug: Fixed not found xib on metadata

--- Phantom/Classes/Modules/MetaData/View/MetaDataView.swift
+++ Phantom/Classes/Modules/MetaData/View/MetaDataView.swift
@@ -8,23 +8,11 @@
 
 import UIKit
 
-class MetaDataView: ViewController {
-
-    @IBOutlet private weak var tableView: UITableView!
-
-    // swiftlint:disable:next weak_delegate
-    var fullDelegate: UITableViewFullDelegate?
-
-    var sections: [UITableView.Section] = [] {
-        didSet {
-            fullDelegate?.sections = sections
-        }
-    }
+class MetaDataView: TableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Color.lighterGrey
-        setUpTable()
     }
 
     override func setUpNavigation() {
@@ -33,13 +21,6 @@ class MetaDataView: ViewController {
             navigationController?.navigationBar.prefersLargeTitles = false
             navigationItem.largeTitleDisplayMode = .never
         }
-    }
-
-    private func setUpTable() {
-        fullDelegate = UITableViewFullDelegate()
-        tableView.fullDelegate = fullDelegate
-        fullDelegate?.sections = sections
-        tableView.tableFooterView = UIView()
     }
 
 }
