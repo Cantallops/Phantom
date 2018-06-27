9bb515aef8d1bfc7ed8581bc472861718ef5ca20
Alberto Cantallops <alberto.cantallops@gmail.com>
2018-06-27 18:43:29 +0200

    :bookmark: Bump to v1.0.12

--- PhantomTests/Common/Tools/Bundle/BundleTest.swift
+++ PhantomTests/Common/Tools/Bundle/BundleTest.swift
@@ -23,10 +23,10 @@ class BundleTest: XCTestCase {
     }
 
     func testVersionNumber() {
-        XCTAssertEqual(bundle.versionNumber, "1.0.11")
+        XCTAssertEqual(bundle.versionNumber, "1.0.12")
     }
 
     func testBuildNumber() {
-        XCTAssertEqual(bundle.buildNumber, "1.0.11.0")
+        XCTAssertEqual(bundle.buildNumber, "1.0.12.0")
     }
 }
